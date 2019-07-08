888e799388e0c84495f8ef580ba68dbf36cebe1c
ramit29 <ramitb@rocketmail.com>
2019-07-08 04:40:45 -0400

    adaboost classifier

--- grid_search/AB_gridsearch.py
+++ grid_search/AB_gridsearch.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import cross_val_score, GridSearchCV, cross_validate, train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.neural_network import MLPClassifier
@@ -6,7 +7,6 @@ import csv
 from sklearn.preprocessing import label_binarize
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv('processed_datasets/final_32classes.csv')
 # Separate out the x_data and y_data.
@@ -23,34 +23,36 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
-knn = KNeighborsClassifier()
-knn.fit(x_train, y_train)
 
-y_pred_train = knn.predict(x_train)
-y_pred_test = knn.predict(x_test)
 
-print("classifier", knn)
+ab = AdaBoostClassifier()
+ab.fit(x_train, y_train)
+
+y_pred_train = ab.predict(x_train)
+y_pred_test = ab.predict(x_test)
+
+print("classifier", ab)
 print ("Accuracy on Train Set")
-print (knn.score(x_train, y_train))
+print (ab.score(x_train, y_train))
 print ("MLP Classifier")
 print ("Accuracy on Test Set")
-print (knn.score(x_test, y_test))
+print (ab.score(x_test, y_test))
 print ("Report")
-print (classification_report(y_test,knn.predict(x_test)))
+print (classification_report(y_test,ab.predict(x_test)))
+
 
 param_grid = {
-    'n_neighbors':[5,6,7,8,9,10],
-         'leaf_size':[1,2,3,5],
-            'weights':['uniform', 'distance'],
-            'algorithm':['auto', 'ball_tree','kd_tree','brute'],
-        #  'n_jobs':[-1]
+
+    'n_estimators': [1000,2000,3000],
+    'learning_rate':[1.0,5.0,10.0],
+    'algorithm':['SAMME.R','SAMME']
 
 }
 
 #,2000
 #,70
 
-grid_search = GridSearchCV(knn, param_grid=param_grid,n_jobs=-1,cv=10)
+grid_search = GridSearchCV(ab, param_grid=param_grid,n_jobs=-1,cv=10)
 
 grid_search.fit(x_train,y_train)
 print(grid_search.best_params_)
