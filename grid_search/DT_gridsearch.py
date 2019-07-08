67e3a0999cd470ef6bfc725784f4d62ec100028c
ramit29 <ramitb@rocketmail.com>
2019-07-08 02:38:30 -0400

    decision tre grid search

--- grid_search/DT_gridsearch.py
+++ grid_search/DT_gridsearch.py
@@ -6,8 +6,7 @@ import csv
 from sklearn.preprocessing import label_binarize
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.tree import DecisionTreeClassifier
 
 data = pd.read_csv('processed_datasets/final_32classes.csv')
 # Separate out the x_data and y_data.
@@ -24,34 +23,34 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
+dt = DecisionTreeClassifier()
+dt.fit(x_train, y_train)
 
+y_pred_train = dt.predict(x_train)
+y_pred_test = dt.predict(x_test)
 
-rf = RandomForestClassifier()
-rf.fit(x_train, y_train)
-
-y_pred_train = rf.predict(x_train)
-y_pred_test = rf.predict(x_test)
-
-print("classifier", rf)
+print("classifier", dt)
 print ("Accuracy on Train Set")
-print (rf.score(x_train, y_train))
+print (dt.score(x_train, y_train))
 print ("MLP Classifier")
 print ("Accuracy on Test Set")
-print (rf.score(x_test, y_test))
+print (dt.score(x_test, y_test))
 print ("Report")
-print (classification_report(y_test,rf.predict(x_test)))
+print (classification_report(y_test,dt.predict(x_test)))
 
 param_grid = {
-    #'n_estimators': [1,1000,2000,3000],
-    #'max_depth': [50,60,70,80,90,100],
-    'criterion':['gini','entropy']
+    'max_features': ['auto', 'sqrt', 'log2',None],
+        'min_samples_split': [2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+          'min_samples_leaf':[1,2,3,4,5,6,7,8,9,10,11],
+         'random_state':[123,345,None],
+         'max_depth':[5,10,15,20,25,None]
 
 }
 
 #,2000
 #,70
 
-grid_search = GridSearchCV(rf, param_grid=param_grid,n_jobs=-1,cv=10)
+grid_search = GridSearchCV(dt, param_grid=param_grid,n_jobs=-1,cv=10)
 
 grid_search.fit(x_train,y_train)
 print(grid_search.best_params_)
